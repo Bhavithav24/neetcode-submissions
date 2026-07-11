c06cb57784effe93c40101d6bbfcd35cfff3945f
Dumpala Bhavithav <bhavithavtech11@gmail.com>
2026-07-11 10:27:19 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0) return 0;
+        Arrays.sort(nums);int max =1;int l=1;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]) continue;
+            if(nums[i]+1==nums[i+1]){
+                max = Math.max(++l,max);
+            }
+            else{
+                l=1;
+            }
+        }
+        return max;
+    }
+}
