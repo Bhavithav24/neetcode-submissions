e9f5c9563b851709aa05cc1286e2f6adda6eef27
Dumpala Bhavithav <bhavithavtech11@gmail.com>
2026-07-13 09:21:02 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int max = 0;
+        for(int j=1;j<prices.length;j++){
+            if(prices[j]<min){
+                min = prices[j];
+            }
+            int prof = prices[j]-min;
+            if(max < prof){
+                max = prof;
+            }
+        }
+        
+        return max;
+    }
+}
